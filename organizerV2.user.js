879e93af7286bb87894f75c9d925731c3a9e35e1
Ivan Kirov <ikirov94@gmail.com>
2025-05-28 14:52:19 +0300

    incremented version

--- organizerV2.user.js
+++ organizerV2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OrganiserV2
 // @namespace    http://tampermonkey.net/
-// @version      2.2
+// @version      2.3
 // @description  Floating UI with buttons that send different POST requests for gear sets or actions, with labels and grouping
 // @author       Peekaboo
 // @match        https://prisonstruggle.com/*
