c571cd7b6540b2ce6f51aacd34acb86c2bea9d93
Ivan Kirov <ikirov94@gmail.com>
2025-05-28 12:41:21 +0300

    fixed namespace

--- organizerV2.user.js
+++ organizerV2.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         OrganiserV2
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/ivan-kirov/prometheus
 // @version      1.6
 // @description  Floating UI with buttons that send different POST requests for gear sets or actions, with labels and grouping
 // @author       Peekaboo
